6d94c2142380a17f084b7f741ade8e71e4949bd1
Harkirat Bansal <hsb.singh@hotmail.com>
2026-09-07 13:05:41 -0400

    Set up Maven project for SDET-001

---
+++ src/main/java/com/harkirat/App.java
@@ -0,0 +1,13 @@
+package com.harkirat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
