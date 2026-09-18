a0872b8ddcf99137e6d80fa8510cc225b2ed9234
Harkirat Bansal <102486559+hsbansal@users.noreply.github.com>
2026-09-18 14:49:51 -0400

    Update price validation in subtotalCalculator method

--- src/main/java/com/harkirat/CartSubtotalCalculator.java
+++ src/main/java/com/harkirat/CartSubtotalCalculator.java
@@ -6,16 +6,17 @@ public class CartSubtotalCalculator {
 
     public static long subtotalCalculator(int[] prices) {
         long subtotal = 0;
+        
         if (prices != null) {
             for (int price : prices) {
-                if (price > -1) {
+                if (price >= 0) {
                     subtotal = price + subtotal;
                 } else {
                     throw new IllegalArgumentException("Invalid price = " + price);
                 }
             }
         } else {
-            throw new IllegalArgumentException("Array is null " + Arrays.toString(prices));
+            throw new IllegalArgumentException("Prices array must not be null");
         }
         return subtotal;
     }
